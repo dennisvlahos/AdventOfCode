adc8a60c7d6f7706e89bc251e7d1e19dc5a86dcb
Dennis Vlahos <dennisvlahos@gmail.com>
2022-12-05 15:47:21 +0200

    Prepare Day 5

---
+++ 2022/day_5.py
@@ -0,0 +1,22 @@
+"""
+Advent of Code 2022 - Day 5
+<https://adventofcode.com/2022/day/5>
+"""
+
+import utils
+
+
+def main() -> int:
+    """
+    Calculates and prints both parts of the puzzle.
+
+    Returns:
+        The exit code.
+    """
+    data = utils.get_data(__file__)
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
